9aa3d71af55914ee2a78c4e6eba018474cda208a
Thorsten Weber <cpt.chaos83@googlemail.com>
2021-12-15 19:46:52 +0100

    initial commit, day 1

---
+++ 1/1.mjs
@@ -0,0 +1,32 @@
+import fs from "fs/promises";
+import path from "path";
+
+const run = async () => {
+  const input = (
+    await fs.readFile(path.resolve(process.cwd(), ".\\1\\input.txt"))
+  )
+    .toString()
+    .split("\n")
+    .filter(Boolean)
+    .map((s) => parseInt(s, 10));
+  const result = input.reduce(
+    (carry, current, index, array) => {
+      if (index === 0) {
+        console.log(current + " ()");
+        return 0;
+      }
+      const prev = array[index - 1];
+      const add = current > prev ? 1 : 0;
+      console.log(`${current} (${add ? "increased" : ""})`);
+      return carry + add;
+    },
+    0,
+    input
+  );
+
+  console.log(
+    `${result} measurements are larger than the previous measurement`
+  );
+};
+
+run().catch((e) => console.error(e));
